7cee07fdaafa08c094a8a9c8815f138fe8f4bb21
liuxynb <2096321485@qq.com>
2023-10-08 12:53:20 +0800

    my work on lab1_1 is done

--- kernel/strap.c
+++ kernel/strap.c
@@ -12,7 +12,8 @@
 //
 // handling the syscalls. will call do_syscall() defined in kernel/syscall.c
 //
-static void handle_syscall(trapframe *tf) {
+static void handle_syscall(trapframe *tf)
+{
   // tf->epc points to the address that our computer will jump to after the trap handling.
   // for a syscall, we should return to the NEXT instruction after its handling.
   // in RV64G, each instruction occupies exactly 32 bits (i.e., 4 Bytes)
@@ -22,18 +23,20 @@ static void handle_syscall(trapframe *tf) {
   // kernel/syscall.c) to conduct real operations of the kernel side for a syscall.
   // IMPORTANT: return value should be returned to user app, or else, you will encounter
   // problems in later experiments!
-  panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
-
+  // panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
+  do_syscall(tf->regs.a0, tf->regs.a1, tf->regs.a2, tf->regs.a3, tf->regs.a4, tf->regs.a5, tf->regs.a6, tf->regs.a7);
 }
 
 //
 // kernel/smode_trap.S will pass control to smode_trap_handler, when a trap happens
 // in S-mode.
 //
-void smode_trap_handler(void) {
+void smode_trap_handler(void)
+{
   // make sure we are in User mode before entering the trap handling.
   // we will consider other previous case in lab1_3 (interrupt).
-  if ((read_csr(sstatus) & SSTATUS_SPP) != 0) panic("usertrap: not from user mode");
+  if ((read_csr(sstatus) & SSTATUS_SPP) != 0)
+    panic("usertrap: not from user mode");
 
   assert(current);
   // save user process counter.
@@ -41,12 +44,15 @@ void smode_trap_handler(void) {
 
   // if the cause of trap is syscall from user application.
   // read_csr() and CAUSE_USER_ECALL are macros defined in kernel/riscv.h
-  if (read_csr(scause) == CAUSE_USER_ECALL) {
+  if (read_csr(scause) == CAUSE_USER_ECALL)
+  {
     handle_syscall(current->trapframe);
-  } else {
+  }
+  else
+  {
     sprint("smode_trap_handler(): unexpected scause %p\n", read_csr(scause));
     sprint("            sepc=%p stval=%p\n", read_csr(sepc), read_csr(stval));
-    panic( "unexpected exception happened.\n" );
+    panic("unexpected exception happened.\n");
   }
 
   // continue (come back to) the execution of current process.
