9197552741494da15926551126fe49f6af3eeef5
liuxynb <2096321485@qq.com>
2024-03-28 16:31:15 +0800

    seem to be finished

--- user/app_help.c
+++ user/app_help.c
@@ -3,17 +3,11 @@
 int main(int argc, char *argv[])
 {
     printu("*************** help test ***************\n");
-    // printu("pwd: print current working directory\n");
-    // printu("cd: change directory\n");
-    // printu("ls: list files in current directory\n");
-    // printu("cat: print content of a file\n");
-    // printu("echo: print a string\n");
-    // printu("mkdir: create a directory\n");
-    // printu("backtrace: print_bracktrace test\n");
-    // printu("cow: copy-on-write test\n");
-    // printu("error_line: error_line test\n");
-    // printu("pf: page fault test\n");
-    // printu("sem: semaphore test\n");
+    // printu("backtrace");
+    // printu("cow");
+    // printu("errorline");
+    // printu("pagefault");
+    // printu("sem");
     exit(0);
     return 0;
 }
